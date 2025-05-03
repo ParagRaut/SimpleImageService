b7342e8a0ab1225a11882a009c1171dfeee8d676
Parag <parag.raut@outlook.com>
2025-05-03 17:17:15 +0200

    Initial commit

---
+++ ImageService.cs
@@ -0,0 +1,34 @@
+﻿using ImageMagick;
+
+public static class ImageService
+{
+    public static byte[] ProcessImage(byte[] imageBytes, uint? width = null, uint? height = null, uint? rotation = null)
+    {
+        using var image = new MagickImage(imageBytes);
+
+        if (width.HasValue && height.HasValue)
+            image.Resize(width.Value, height.Value);
+
+        if (rotation.HasValue)
+            image.Rotate(rotation.Value);
+
+        image.Quality = 75;
+
+        return image.Format is MagickFormat.Heic or MagickFormat.Heif
+            ? image.ToByteArray(MagickFormat.Jpeg)
+            : image.ToByteArray();
+    }
+
+    public static async Task<byte[]> ProcessImageFromUrlAsync(HttpClient httpClient, string url, uint? width = null, uint? height = null, uint? rotation = null)
+    {
+        var imageBytes = await httpClient.GetByteArrayAsync(url).ConfigureAwait(false);
+
+        return ProcessImage(imageBytes, width, height, rotation);
+    }
+
+    public static byte[] ProcessImageFromFile(string filePath, uint? width = null, uint? height = null, uint? rotation = null)
+    {
+        var imageBytes = File.ReadAllBytes(filePath);
+        return ProcessImage(imageBytes, width, height, rotation);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Diagnosers;
+
+BenchmarkRunner.Run<ImageServiceBenchmark>();
+
+[ShortRunJob(RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+[HideColumns("Error", "StdDev", "RatioSD")]
+public class ImageServiceBenchmark
+{
+    private byte[] _imageBytes;
+    private const string _filePath = @"/Users/paragraut/Downloads/shelf-christmas-decoration.heic"; // Use a valid image path
+    private HttpClient _httpClient;
+    private const string _imageUrl = "https://picsum.photos/800/900"; // Use a valid image URL
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _imageBytes = File.ReadAllBytes(_filePath);
+        _httpClient = new HttpClient();
+    }
+
+    [Benchmark(Baseline = true)]
+    public byte[] ProcessImage() =>
+        ImageService.ProcessImage(_imageBytes, 200, 200, 90);
+
+    [Benchmark]
+    public byte[] ProcessImageFromFile() =>
+        ImageService.ProcessImageFromFile(_filePath, 200, 200, 90);
+
+    [Benchmark]
+    public async Task<byte[]> ProcessImageFromUrlAsync() =>
+        await ImageService.ProcessImageFromUrlAsync(_httpClient, _imageUrl, 200, 200, 90);
+}
